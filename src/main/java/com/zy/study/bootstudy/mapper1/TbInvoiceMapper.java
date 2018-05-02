0a1eb1d7362647f84f383e02cec280e64d74fd4d
zy <1270157082@qq.com>
2018-05-03 00:28:38 +0800

    通用mapper

---
+++ src/main/java/com/zy/study/bootstudy/mapper1/TbInvoiceMapper.java
@@ -0,0 +1,7 @@
+package com.zy.study.bootstudy.mapper1;
+
+import com.zy.study.bootstudy.entity1.TbInvoice;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface TbInvoiceMapper extends Mapper<TbInvoice> {
+}
